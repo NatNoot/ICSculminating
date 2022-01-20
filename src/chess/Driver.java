cc62ad929c882271e673adc2d156f45ef29b1b58
334736790 <334736790@C-2UA4081328.tdsb.on.ca>
2022-01-20 13:06:16 -0500

    Making scene play object oriented

--- src/chess/Driver.java
+++ src/chess/Driver.java
@@ -6,7 +6,22 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		//TESTING
-		GeneralScene.openingScene(args);
+		String ans = GeneralScene.openingScene(args);
+		
+		if (ans.equals("n")) {
+			GeneralScene.sceneOneA();
+			GeneralScene.sceneTwoA();
+			GeneralScene.sceneThreeA();
+			GeneralScene.sceneFourA();
+			GeneralScene.sceneFiveA();
+		}
+		else {
+			GeneralScene.sceneOneB();
+			GeneralScene.sceneTwoB();
+			GeneralScene.sceneThreeB();
+			GeneralScene.sceneFourB();
+			GeneralScene.sceneFiveB();
+		} 
 		}
 	}
 
--- src/chess/GeneralScene.java
+++ src/chess/GeneralScene.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class GeneralScene {
 	
 	
-	public static void openingScene(String[] args) {
+	public static String openingScene(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
 		System.out.println("Knight: Do you have an audience with the King?");
@@ -19,7 +19,38 @@ public class GeneralScene {
 			System.out.println("Knight: Then get lost.");
 		} else {
 			System.out.println("Please answer with y or n.");
-			
 	}
-}
+		return ans;
+	}
+	public static void sceneOneA() {
+		System.out.println("System: succesfully entered queen pathway");
+	}
+	public static void sceneTwoA() {
+		System.out.println("System: succesfully entered queen scene two");
+	}
+	public static void sceneThreeA() {
+		System.out.println("System: succesfully entered queen scene three");
+	}
+	public static void sceneFourA() {
+		System.out.println("System: succesfully entered queen scene four");
+	}
+	public static void sceneFiveA() {
+		System.out.println("System: succesfully entered queen scene five");
+	}
+		
+	public static void sceneOneB() {
+		System.out.println("System: succesfully entered king pathway");
+	}
+	public static void sceneTwoB() {
+		System.out.println("System: succesfully entered king scene two");
+	}
+	public static void sceneThreeB() {
+		System.out.println("System: succesfully entered king scene three");
+	}
+	public static void sceneFourB() {
+		System.out.println("System: succesfully entered king scene four");
+	}
+	public static void sceneFiveB() {
+		System.out.println("System: succesfully entered king scene five");
+	}
 }
