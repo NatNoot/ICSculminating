9435a94d25a57286fdd60e99ad0e5beca64f1235
Natalie <Natalie@192.168.2.120>
2022-01-21 12:53:13 -0500

    Made the file reading & printing more user friendly, QueenLine

--- src/chess/QueenLine.java
+++ src/chess/QueenLine.java
@@ -1,20 +1,29 @@
 package chess;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class QueenLine {
 	
 	public static void main() throws Exception{
-		String temp = "";
+		ArrayList<String> lines = new ArrayList<String>();
 	
 		File file = new File("scene2.txt");
 		Scanner in = new Scanner(file);
 		
 		while (in.hasNextLine()) {
-			String line = in.nextLine();
-			System.out.println(line);
+			lines.add(in.nextLine());
 		}
+		in.close();
+		
+		for (int i = 0; i < lines.size(); i++) {
+			Scanner key = new Scanner(System.in);
+			String nextKey = key.nextLine();
+			System.out.println(lines.get(i));
+			System.out.println("Press Enter to Continue");
+		}
+		
 	
 	}
 }
